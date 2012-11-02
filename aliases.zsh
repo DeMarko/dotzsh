52f5153af0e3a3fdb9473bd8a1d57f52c30235b1
Dannel Jurado <djurado@etsy.com>
2012-11-02 19:10:43 +0000

    update some vars and aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,9 +4,9 @@
 alias man='LC_ALL=C LANG=C man'
 
 if [ `uname` = "Darwin" ]; then
-    alias ls='ls -AFGC'
+    alias ls='ls -FGC'
 else
-    alias ls='ls -AFGC --color=auto'
+    alias ls='ls -FGC --color=auto'
 fi
 
 alias ll='ls -al'
@@ -41,7 +41,8 @@ alias clearviews="rm $HOME/.vim/tmp/views/*"
 # Etsy stuff
 if [[ $HOSTNAME =~ ^.*\.ny4(dev)?\.etsy\.com$ ]]; then
     alias apacherestart="sudo /etc/init.d/httpd restart"
-    alias cddw="cd ~/development/Web"
+    alias cddw="cd ~/development/Etsyweb"
     alias mcflush="sudo /etc/init.d/memcached restart"
+    alias hiphop="~/development/Etsyweb/bin/hphp-data/run-hp.sh"
     eval $(dbaliases)
 fi
